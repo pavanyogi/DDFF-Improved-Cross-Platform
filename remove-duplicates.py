2a0ecf88b0ff20562df1273cda3cba3fea74caea
Pavan Yogi <pavanyogi121@gmail.com>
2022-03-28 17:03:20 +0530

    Add script to delete files/folders in a loop as per user input choices

---
+++ remove-duplicates.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+# from os import path
+import os, shutil
+from colorama import Fore, Back, Style
+
+
+def remove(path):
+    """ param <path> could either be relative or absolute. """
+    if os.path.isfile(path) or os.path.islink(path):
+        os.remove(path)  # remove the file
+    elif os.path.isdir(path):
+        shutil.rmtree(path)  # remove dir and all contains
+    else:
+        raise ValueError("file {} is not a file or dir.".format(path))
+    print('remove done')
+
+
+if __name__ == "__main__":
+	# Open a file: f1
+	# f1 = open(r'test.txt',mode='r')
+	# f1 = open(r'ddff-wip.txt',mode='r')
+	f1 = open(r'ddff.txt',mode='r')
+	 
+	# read all lines at once
+	file_content = f1.read()
+	 
+	# close the file
+	f1.close()
+
+	# print('file_content', file_content)
+
+	item_list = file_content.split('\n\n')
+
+	# print('item_list', item_list)
+
+	for item in item_list:
+		# print(item)
+		# print()
+		item_lines = item.split('\n')
+		# print('len(item_lines)', len(item_lines))
+		tmp_list = []
+		for line in item_lines:
+			# print(line)
+			if line.startswith('/') and os.path.exists(line):
+				# print(line)
+				tmp_list.append(line)
+			elif line.startswith('*'):
+				# print(line)
+				tmp_list.append(line)
+		# print('len(tmp_list)', len(tmp_list))
+		# print()
+		your_input = -1
+		if len(tmp_list) > 2:
+			tmp_list2 = []
+			for k in tmp_list:
+				if tmp_list.index(k) != 0:
+					print(Fore.GREEN + str(tmp_list.index(k)), Fore.GREEN + str(k))
+				else:
+					print(k)
+				tmp_list2.append(tmp_list.index(k))
+			user_input = input(Fore.RED + "keep index: ")
+			print()
+			if user_input.isdigit():
+				user_input = int(user_input)
+			if user_input != 0 and user_input in tmp_list2:
+				your_input = user_input
+			if your_input != -1:
+				for l in tmp_list:
+					if tmp_list.index(l) != your_input and tmp_list.index(l) != 0:
+						print(Fore.RED + 'delete', Fore.RED + str(tmp_list.index(l)),  str(l))
+						if os.path.exists(l):
+							remove(l)
+					elif tmp_list.index(l) == 0:
+						print(Fore.GREEN + str(l))
+					else:
+						print(Fore.GREEN + 'keep', Fore.GREEN + str(tmp_list.index(l)), Fore.GREEN + str(l))
+				print()
+			input()
+		
